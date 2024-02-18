c12974d98df4dbf4996e31cb389dee3c7e67f7cd
Shahd Osman <Shahd_aa@live.com>
2024-02-18 17:40:50 -0500

    Add Java Project

---
+++ src/main/java/HelloPassengers.java
@@ -0,0 +1,8 @@
+
+public class HelloPassengers {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome in Software Development Express Train!");
+		System.out.println("Hello!");
+	}
+}
