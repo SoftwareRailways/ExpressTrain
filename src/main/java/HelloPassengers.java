6a95a61e0be9d6e7c1a3c2ef8cb7e6e3ee18d58e
Mayssa Alsaid <misoonaaalsaid@gmail.com>
2024-02-20 01:32:49 -0500

    Adding  greeting from Mayssa Alsaid

--- src/main/java/HelloPassengers.java
+++ src/main/java/HelloPassengers.java
@@ -4,5 +4,7 @@ public class HelloPassengers {
 	public static void main(String[] args) {
 		System.out.println("Welcome in Software Development Express Train!");
 		System.out.println("Hello!");
+		System.out.println("Hello! I'm Mayssa Alsaid from NH");
+
 	}
 }
